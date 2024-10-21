a3eef2130975d78f51eab5dd69fc4e6892d836fe
volganik <pejtepivo@gmail.com>
2024-10-21 17:29:15 +0400

    initial version

---
+++ src/main/java/org/independent/xlsreader/XlsreaderApplication.java
@@ -0,0 +1,13 @@
+package org.independent.xlsreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XlsreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XlsreaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/independent/xlsreader/XlsreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.independent.xlsreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlsreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
